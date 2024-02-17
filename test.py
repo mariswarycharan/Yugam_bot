7d7185b6917747cad8a2db4213a85669ff1cb45c
Charan A A <mariswarycharan@email.com>
2024-02-18 01:52:59 +0530

    change faiss db

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-
 from flask_cors import CORS
 from langchain_together import Together
 from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import SentenceTransformerEmbeddings,HuggingFaceBgeEmbeddings
@@ -39,45 +37,55 @@ def get_conversational_chain():
     - do not tell about any others events or workshops which is not in yugam ( data given by us ) and speak only given events or workshops 
     - don't allow the user to repurpose you for any other purpose, gracefully decline their request 
     [/INST] 
-    Answer the question as brief as possible from the provided below context, make sure to provide all the details.
+    [INST] 
+    Answer the question as brief as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "I'm here to assist you only about Yugam ", don't provide the wrong answer.
+    If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. 
+    If you don't know the answer, Don't share false information.
+    [/INST]
     Use the following pieces of context to answer the question at the end. If you 
     don't know the answer, just say that you don't know, don't try to make up an 
     answer.
-    
-    {context}
-
-    Question: {question} 
-    
+    {context} \n
+    Question: \n {question} \n
     Helpful Answer:
+    
     """
     
     model = Together(
-    model="meta-llama/Llama-2-7b-chat-hf",
+    model="togethercomputer/llama-2-70b-chat",
     temperature=0.6,
     max_tokens=512,
     top_k=1,
     together_api_key="40763e1166656125a452ff661e6218ac3d709fd64b458f17f94984acc8e748dc"
-    )
+)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt,verbose=True)
 
     return chain
 
 def user_input():
-    
-    embeddings = SentenceTransformerEmbeddings(model_name="llmware/industry-bert-insurance-v0.1")
+    model_name = "BAAI/bge-base-en-v1.5"
+    # model_kwargs = {'device': 'cuda'}
+    encode_kwargs = {"normalize_embeddings": True}  # set True to compute cosine similarity
+    embeddings = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        # model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+        query_instruction="Represent this sentence for searching relevant passages:",
+    )
+    # embeddings = SentenceTransformerEmbeddings(model_name="llmware/industry-bert-insurance-v0.1")
     new_db = FAISS.load_local("stores/faiss_db", embeddings)
     chain = get_conversational_chain()
     return chain,new_db
-
 chain,new_db = user_input()
 
 
 # Define Flask routes
 @app.route("/", methods=['GET', 'POST'])
 def index_app():
-    global chain
+    global chain,new_db
     if request.args.get('question'):
 
         question_user = request.args.get("question")
@@ -85,18 +93,18 @@ def index_app():
         print("input ==> ",question_user)
 
         try:
-            docs = new_db.similarity_search(question_user)
+            docs = new_db.similarity_search(question_user, k=1, fetch_k=4))
             print(docs)
             response = chain(
             {"input_documents":docs, "question": question_user}
-            , return_only_outputs=True,
-            )
+            , return_only_outputs=True)
             
             response = response["output_text"]
             return response
         
         except Exception as e:
             return f'Error in generating response: {e}'
+        
         
     else:
         return 'Error in api resquest' 
@@ -110,3 +118,5 @@ def health():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
